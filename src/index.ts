eb740c33883d52b0d2bb759b2956110753fa089b
NadiaElso <92063948+NadiaElso@users.noreply.github.com>
2021-10-24 19:49:15 -0300

    ahorafunciona se agrega la variable value en el evento del boton

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,20 @@
-let sueldo: number = document.getElementById("sueldo");
-
-let btn = document.getElementById("btn");
+let sueldo: any = document.getElementById("sueldo");
+let btn: any = document.getElementById("btn");
 btn.addEventListener("click", () => {
-  switch (sueldo) {
-    case 1:
-      sueldo = 15000 || sueldo < 15.0;
+  let sueldo2 = Number(sueldo.value);
+  switch (true) {
+    case sueldo2 > 0 && sueldo2 <= 15000:
       console.log("20%");
       break;
-    case 2:
-      sueldo = 15.001 || sueldo < 20.0;
+    case sueldo2 > 15000 && sueldo2 <= 20000:
       console.log("10%");
       break;
-    case 3:
-      sueldo = 20.001 || sueldo < 25.0;
+    case sueldo2 > 20001 && sueldo2 <= 25000:
       console.log("5%");
       break;
     default:
-    case 4:
-      sueldo > 25.0;
+    case sueldo2 > 25000:
       console.log("No hay descuento");
+      break;
   }
 });
